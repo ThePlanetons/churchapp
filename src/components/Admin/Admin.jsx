ca17b3211b0e6206d84b51234374c1b07fb1db23
Akashmoorthi7 <akashmoorthi7@gmail.com>
2025-01-06 22:26:31 +0530

    [member]Admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./Sidebar/Sidebar";
 
 export function Admin() {
   return (
-    <div className='flex flex-row w-full'>
+    <div className='flex flex-row w-full h-screen'>
       <div className="flex w-full bg-gray-100">
         <Sidebar />
 
