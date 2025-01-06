cf080a5c99dc64a3bdb4e41b6877662d030d8406
Akashmoorthi7 <akashmoorthi7@gmail.com>
2025-01-06 14:09:27 +0530

    [push]Admin

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,19 +1,10 @@
 import { Dashboard } from "./Dashboard/Dashboard";
-import { Sidebar } from "./Member/sidebar/Sidebar";
-import { MemberHeader } from "./Member/member/MemberHeader";
 import { MemberContent } from "./Member/member/MemberContent";
 
 export function Admin() {
   return (
-    <div className='flex flex-row'>
-      {/* <Dashboard /> */}
-      <div className="flex h-screen bg-gray-100">
-         <Sidebar />
-           <div className="flex-1 overflow-auto">
-             <MemberHeader />
-             <MemberContent />
-           </div>
-      </div>
+    <div>
+       <MemberContent />
     </div>
   );
 }
--- src/components/Admin/Member/member/MemberContent.jsx
+++ src/components/Admin/Member/member/MemberContent.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { PenSquare } from 'lucide-react';
 import { MemberList } from './MemberList';
 import { useMemberData } from '../../hooks/useMemberData';
+import { Sidebar } from '../sidebar/Sidebar';
+import { MemberHeader } from './MemberHeader';
 
 export function MemberContent() {
   const { members } = useMemberData();
 
   return (
-    <div className="p-4 sm:p-6 lg:p-10 lg:">
-      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-6">
-        <h3 className="text-base sm:text-lg lg:text-xl font-medium mb-4 sm:mb-0">
-          Member List
-        </h3>
-        <button className="flex items-center px-3 sm:px-4 py-2 bg-amber-100 text-amber-900 rounded-lg text-sm sm:text-base">
-          <PenSquare className="w-4 h-4 sm:w-5 sm:h-5 mr-2" />
-          Add Member
-        </button>
+    <div className="flex flex-col md:flex-row h-screen"> 
+      <Sidebar /> 
+      <div className="flex-1 overflow-auto bg-gray-100"> 
+        <MemberHeader />
+        <div className="p-4 sm:p-6 lg:p-10"> 
+          <div className="flex flex-col sm:flex-row justify-between items-center mb-6">
+            <h3 className="text-base sm:text-lg lg:text-xl font-medium mb-4 sm:mb-0">
+              Member List
+            </h3>
+            <button className="flex items-center px-3 sm:px-4 py-2 bg-amber-100 text-amber-900 rounded-lg text-sm sm:text-base">
+              <PenSquare className="w-4 h-4 sm:w-5 sm:h-5 mr-2" />
+              Add Member
+            </button>
+          </div>
+          <MemberList members={members} />
+        </div>
       </div>
-      <MemberList members={members} />
     </div>
   );
 }
